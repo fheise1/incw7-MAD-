038fa874ad028d2ca53a6473be3400123174d199
Isaac Lara <isaac.lara014@gmail.com>
2025-02-26 16:44:10 -0500

    fixed merge conflicts

--- incw7/lib/main.dart
+++ incw7/lib/main.dart
@@ -54,7 +54,7 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Fading Text Animation'),
+        title: Text('Fading Text Animation Page 1'),
         actions: <Widget>[
           Switch(
             value: isDarkMode,
@@ -69,15 +69,104 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
         ],
       ),
       body: Center(
-        child: GestureDetector(
-          onTap: toggleVisibility,
-          child: AnimatedOpacity(
-            opacity: _isVisible ? 1.0 : 0.0,
-            duration: const Duration(seconds: 1),
-            curve: Curves.bounceInOut,
-            child:
-                const Text('Hello, Flutter!', style: TextStyle(fontSize: 24)),
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: const Text('Go to Page 2'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          SecondRoute(toggleTheme: widget.toggleTheme)),
+                );
+              },
+            ),
+            SizedBox(height: 300),
+            GestureDetector(
+              onTap: toggleVisibility,
+              child: AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: Duration(seconds: 1),
+                curve: Curves.bounceInOut,
+                child: const Text(
+                  'Hello, Flutter!',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleVisibility,
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatefulWidget {
+  final Function(ThemeMode) toggleTheme;
+
+  SecondRoute({required this.toggleTheme});
+
+  @override
+  _SecondRouteState createState() => _SecondRouteState();
+}
+
+class _SecondRouteState extends State<SecondRoute> {
+  bool _isVisible = true;
+
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fading Text Animation Page 2'),
+        actions: <Widget>[
+          Switch(
+            value: isDarkMode,
+            onChanged: (isOn) {
+              if (isOn) {
+                widget.toggleTheme(ThemeMode.dark);
+              } else {
+                widget.toggleTheme(ThemeMode.light);
+              }
+            },
           ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: const Text('Go to Page 1'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            SizedBox(height: 300),
+            GestureDetector(
+              onTap: toggleVisibility,
+              child: AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: Duration(seconds: 1),
+                curve: Curves.decelerate,
+                child: const Text(
+                  'Hello, Flutter!',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
