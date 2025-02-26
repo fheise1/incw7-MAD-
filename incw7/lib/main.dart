3c5309695bf7777fbf3d3607bb799282f311dde3
Isaac Lara <isaac.lara014@gmail.com>
2025-02-26 14:16:39 -0500

    added theme mode toggle

--- incw7/lib/main.dart
+++ incw7/lib/main.dart
@@ -4,16 +4,37 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  void _toggleTheme(ThemeMode themeMode) {
+    setState(() {
+      _themeMode = themeMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FadingTextAnimation(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: _themeMode,
+      home: FadingTextAnimation(toggleTheme: _toggleTheme),
     );
   }
 }
 
 class FadingTextAnimation extends StatefulWidget {
+  final Function(ThemeMode) toggleTheme;
+
+  FadingTextAnimation({required this.toggleTheme});
+
   @override
   _FadingTextAnimationState createState() => _FadingTextAnimationState();
 }
@@ -29,17 +50,33 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
 
   @override
   Widget build(BuildContext context) {
+    bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Fading Text Animation'),
+        actions: <Widget>[
+          Switch(
+            value: isDarkMode,
+            onChanged: (isOn) {
+              if (isOn) {
+                widget.toggleTheme(ThemeMode.dark);
+              } else {
+                widget.toggleTheme(ThemeMode.light);
+              }
+            },
+          ),
+        ],
       ),
       body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 1),
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
+        child: GestureDetector(
+          onTap: toggleVisibility,
+          child: AnimatedOpacity(
+            opacity: _isVisible ? 1.0 : 0.0,
+            duration: const Duration(seconds: 1),
+            curve: Curves.bounceInOut,
+            child:
+                const Text('Hello, Flutter!', style: TextStyle(fontSize: 24)),
           ),
         ),
       ),
